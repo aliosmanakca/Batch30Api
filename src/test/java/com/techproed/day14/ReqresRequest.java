b1db5e3025880b6099a8a5279437ac9427fbd451
aliosmanakca <akcaaliosman@gmail.com>
2021-12-01 21:05:07 -0500

    .

---
+++ src/test/java/com/techproed/day14/ReqresRequest.java
@@ -0,0 +1,13 @@
+package com.techproed.day14;
+
+import org.junit.Test;
+
+public class ReqresRequest {
+    @Test
+    public void test(){
+
+        ReqresToken obje=new ReqresToken();
+        System.out.println(obje.TokenAl());
+
+    }
+}
